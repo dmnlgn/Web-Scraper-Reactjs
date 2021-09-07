d0876633ccfde95d8a04f8e7309f3082dcad83bb
Damian Łaganowski <damian7507@gmail.com>
2021-09-07 12:47:44 +0200

    fix scraping data

--- server/scraped_pages/fashion/page_two/fragments/tshirts.js
+++ server/scraped_pages/fashion/page_two/fragments/tshirts.js
@@ -1,46 +1,37 @@
 const axios = require('axios');
 const cheerio = require('cheerio');
-let page = 90;
+let page = 1;
+const data = [];
 
 const scraped = async() => {
     const API = `https://answear.com/k/on/odziez/t-shirty-i-polo?page=${page}`;
-    const data = [];
-    const data_color = [];
     const response = axios.get(API)
         .then((response) => {
             let $ = cheerio.load(response.data);
             const stack = $('.container--xl > div > .row > .col-md-4');
+            stack.each((i, el) => {
+                if(i) {
+                    const Name = $(el).find('.ProductItem__productCardDescription__ScKhU > span').text();
+                    const PriceRegular = $(el).find('.ProductItem__productCardDescription__ScKhU > div > div > p').first().text();
+                    const PricePromo = $(el).find('.ProductItem__productCardDescription__ScKhU > div > div > p').first().next().text();
 
-            console.log("page items: " + stack.length + "\n");
-
-            if(stack.length) {
-                stack.each((i, el) => {
-                    if(i) {
-                        const Name = $(el).find('.ProductItem__productCardDescription__ScKhU > span').text();
-                        const PriceRegular = $(el).find('.ProductItem__productCardDescription__ScKhU > div > div > p').first().text();
-                        const PricePromo = $(el).find('.ProductItem__productCardDescription__ScKhU > div > div > p').first().next().text();
-
-                        data.push({
-                            name: Name,
-                            price: PriceRegular,
-                            price_promo: PricePromo,
-                        })
-                    }
-                })
-                page += 1;
-                return scraped();
-            }
-            console.log(data)
+                    data.push({
+                        name: Name,
+                        price: PriceRegular,
+                        price_promo: PricePromo,
+                    })
+                } else {
+                    page += 1;
+                    return scraped();
+                }
+            })
             return data;
         })
         .catch((err) => {
             console.log(err);
         })
-        console.log("page number: " + page)
-        return response;
+    return response;
 }
-
-scraped();
 
 module.exports = { 
     scraped
